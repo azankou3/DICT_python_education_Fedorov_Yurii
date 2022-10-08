566cd47ca9ad514618cdcb9ada2ac62da51db857
Yurii Fedorov <y.o.fedorov@student.khai.edu>
2022-10-08 13:32:49 +0300

    ChatBot 3-rd stage

--- Chatbot/chatbot.py
+++ Chatbot/chatbot.py
@@ -9,6 +9,13 @@ name = input(">")
 discovername = (f"What a great name you have, {name}")
 print(discovername)
 
+guessing_age = ("Let me guess your age? \nEnter remainders of dividing your age by 3, 5 and 7")
+print(guessing_age)
 
+remainder3 =int(input(">"))
+remainder5 =int(input(">"))
+remainder7 =int(input(">"))
 
-
+age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+your_age = (f"Your age is, {age} that`s a good time to start programming!")
+print(your_age)
