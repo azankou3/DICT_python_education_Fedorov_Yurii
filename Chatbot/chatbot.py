7d227560ccc04909ed41705a51544147a03b186d
Yurii Fedorov <y.o.fedorov@student.khai.edu>
2022-10-08 13:30:43 +0300

    ChatBot 2-nd stage

--- Chatbot/chatbot.py
+++ Chatbot/chatbot.py
@@ -1,2 +1,14 @@
 greetings = ("Hello! My name is R2D2 \nI was created in 2022")
 print(greetings)
+
+contact = ("Please remind me your name: ")
+print(contact)
+
+name = input(">")
+
+discovername = (f"What a great name you have, {name}")
+print(discovername)
+
+
+
+
