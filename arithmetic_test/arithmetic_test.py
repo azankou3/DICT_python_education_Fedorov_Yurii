4b5b963cf88d2d51473a2c029bffbcbaaf5f1431
Yurii Fedorov <y.o.fedorov@student.khai.edu>
2023-02-21 15:50:23 +0200

    try to fix

--- arithmetic_test/arithmetic_test.py
+++ arithmetic_test/arithmetic_test.py
@@ -1,36 +1,30 @@
+'''
+Designed by Fedorov
+'''
 import random
 import operator
+from datetime import datetime
 
-class Random_test:
-    def __init__(self):
-        pass
-# #Easy test
-#     def random_number(self):
-#         x = random.randrange(2, 10)
-#         y = random.randrange(2, 10)
-#         sign_list = {'+': operator.add,
-#                      '-': operator.sub,
-#                      '*': operator.mul}
-#         sign = random.choice(list(sign_list.keys()))
-#         res = (x, sign, y)
-#         answer = (sign_list.get(sign)(x, y))
-#         print(*res)
-#         print(answer)
-#         client_input = int(input('>'))
-#         if client_input == answer:
-#             print('Right!')
-#         else:
-#             print('Wrong!')
 
-#Second stage
-    def second_stage(self):
+class RandomTest:
+    '''
+    Main class
+    '''
+
+
+    @staticmethod
+    def second_stage():
+        '''
+        Second Stage of practical
+        '''
         marks = 0
         tests = 0
         number_of_tests = 5
+        value_error = 0
         while tests < number_of_tests:
             tests = tests + 1
-            x = random.randrange(2, 10)
-            y = random.randrange(2, 10)
+            x = random.randint(2, 10)
+            y = random.randint(2, 10)
             sign_list = {'+': operator.add,
                          '-': operator.sub,
                          '*': operator.mul}
@@ -38,74 +32,105 @@ class Random_test:
             res = (x, sign, y)
             answer = (sign_list.get(sign)(x, y))
             print(*res)
-            print(answer)
-            client_input = int(input('>'))
-            if client_input == answer:
-                print('Right!')
-                marks = marks + 1
-            else:
-                print('Wrong!')
+            # print(answer) #Answer for test
+            client_input = input('>')
+            try:
+                if int(client_input) == answer:
+                    print('Right!')
+                    marks = marks + 1
+                elif int(client_input) != answer:
+                    print('Wrong!')
+            except ValueError:
+                print('Incorrect format\nTry numbers')
+                value_error += 1
+                number_of_tests += 1
+
             if tests == number_of_tests:
-                final = (f'Your mark is {marks}/{number_of_tests}')
+                corrected_amount_of_test = number_of_tests - value_error
+                final = (f'Your mark is {marks}/{corrected_amount_of_test}\n'
+                         f'ValueErrors - ({value_error})')
                 print(final)
-                print("Would you like to save your result to file\nEnter\n1.yes\n2.no")
+                print("Would you like to save your result to file\nEnter\nyes or no")
                 results_file_input = input('>')
-                if results_file_input == "1":
-                    with open("Results File.txt","a") as text_write:
+                if results_file_input.lower().strip() == 'yes':
+                    with open("Results File.txt", "a") as text_write:
+                        now = datetime.now()
+                        dt_string = now.strftime('%d/%m/%Y %H:%M:%S')
                         text_input = input("Enter your name: >")
                         text_input_group = input("Enter your group")
                         text_write.write(f"\n\n{text_input} {text_input_group}"
-                                         f" Simple arithmetic operations with numbers from 2 to 9-{final}")
-                else:
-                    Menu.welcome(Menu)
+                                         f" Simple arithmetic operations with"
+                                         f" numbers from 2 to 9-{final} "
+                                         f"\nDate and time of test {dt_string}")
 
 
-#Thir stage
-    def thrid_stage(self):
+
+
+    @staticmethod
+    def third_stage():
+        '''
+        Third stage of practical
+        '''
         marks = 0
         tests = 0
         number_of_tests = 5
+        value_error = 0
         while tests < number_of_tests:
             tests = tests + 1
-            x = random.randrange(11, 29)
+            x = random.randint(11, 29)
             res = (f'{x}^2')
             answer = (x**2)
             print(res)
-            print(answer)
-            client_input = int(input('>'))
-            if client_input == answer:
-                print('Right!')
-                marks = marks + 1
-            else:
-                print('Wrong!')
-            if tests == number_of_tests:
-                final = (f'Your mark is {marks}/{number_of_tests}')
-                print(final)
-                print("Would you like to save your result to file\nEnter\n1.yes\n2.no")
-                results_file_input = input('>')
-                if results_file_input == "1":
-                    with open("Results File.txt", "a") as text_write:
-                        text_input = input("Enter your name: >")
-                        text_input_group = input("Enter your group")
-                        text_write.write(f"\n\n{text_input} {text_input_group}"
-                                         f" Squaring numbers from 11 to 29-{final}")
-                else:
-                    Menu.welcome(Menu)
+            # print(answer) #Answer for test
+            client_input = input('>')
+            try:
+                if int(client_input) == answer:
+                    print('Right!')
+                    marks = marks + 1
+                elif int(client_input) != answer:
+                    print('Wrong!')
+            except ValueError:
+                print('Incorrect format\nTry numbers')
+                value_error += 1
+                number_of_tests += 1
+            try:
+                if tests == number_of_tests:
+                    corrected_amount_of_test = number_of_tests - value_error
+                    final = (f'Your mark is {marks}/{corrected_amount_of_test}\n'
+                             f'ValueErrors - ({value_error})')
+                    print(final)
+                    print("Would you like to save your result to file\nEnter\nyes or no")
+                    results_file_input = input('>')
+                    if results_file_input.lower().strip() == 'yes':
+                        with open("Results File.txt", "a") as text_write:
+                            now = datetime.now()
+                            dt_string = now.strftime('%d/%m/%Y %H:%M:%S')
+                            text_input = input("Enter your name: >")
+                            text_input_group = input("Enter your group")
+                            text_write.write(f"\n\n{text_input} {text_input_group}"
+                                            f" Squaring numbers from 11 to 29-{final}"
+                                            f"\nDate and time of test {dt_string}")
+            except ValueError:
+                print('Please use number')
 
 
 class Menu:
-#Start menu
-    def welcome(self):
+    '''
+    Menu class
+    '''
+
+    @staticmethod
+    def welcome():
         print('Виберіть рівень складності\n'
               '1. Прості арифметичні операції з числами від 2 до 9\n'
               '2. Зведення у квадрат чисел від 11 до 29')
         menu_input = input('>')
         if menu_input == '1':
-            Random_test.second_stage(Random_test)
+            RandomTest.second_stage()
         elif menu_input == '2':
-            Random_test.thrid_stage(Random_test)
+            RandomTest.third_stage()
         else:
             print('Enter only numbers from Menu')
-            Menu.welcome(Menu)
-if __name__ == '__main__':
-    Menu.welcome(Menu)
+
+while True:
+    Menu.welcome()
